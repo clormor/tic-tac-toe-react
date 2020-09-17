a521d63b0af4e1edbc5ce987a0b53976005bc4c7
Chris Lormor <clormor@palantir.com>
2020-09-17 12:31:37 +0100

    Updates

---
+++ precache-manifest.fda9b0221e7a09dea312100d739673ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b70f7176678fb0afcc0633450fbb35e",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "2270088ec6338ba739be",
+    "url": "/tic-tac-toe-react/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "cdc12b09c0ff04d27a36",
+    "url": "/tic-tac-toe-react/static/js/2.4429fed6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-react/static/js/2.4429fed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2270088ec6338ba739be",
+    "url": "/tic-tac-toe-react/static/js/main.ffa08e83.chunk.js"
+  },
+  {
+    "revision": "b3fb02dc9df6095594b3",
+    "url": "/tic-tac-toe-react/static/js/runtime-main.81226336.js"
+  }
+]);
